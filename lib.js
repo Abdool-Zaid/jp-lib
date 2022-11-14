053ad26aed4ae94bf3798b8da5cf778094cfb06d
Abdool-Zaid <aztoorabally7447@gmail.com>
2022-11-14 12:19:30 +0200

    'first-commit'

---
+++ lib.js
@@ -0,0 +1,4 @@
+[
+    { char: "一", alt: [], meaning: ["one","ground"],reading:[{on:'',kun:'',trans:""},], strokes: 1 },
+    // { char: "", alt: [], meaning: [""],reading:[{on:'',kun:'',trans:""},], strokes: "" },
+];
